882fb35567f881cfbef191c8e55a858600fdd93e
Marc Rußwurm <marc.russwurm@tum.de>
2020-09-02 14:18:29 +0200

    tweaks

--- readme.md
+++ readme.md
@@ -6,3 +6,7 @@ start via
 ```
 bash start.sh
 ```
+
+## Added Testing Protocol here
+
+https://docs.google.com/document/d/1cDUTXAt9qs2Pe40jQk3AWiKOhbIUfSz8vw4iAz7QI3g/edit#
--- upload.sh
+++ upload.sh
@@ -1,1 +1,4 @@
-rsync -avz -e "ssh" . ubuntu@138.246.234.218:sen12ms-human-few-shot-classifier
+rsync -avz -e "ssh" \
+  --exclude 'app/static/records.json' \
+  --exclude 'app/static/img' \
+  . ubuntu@138.246.234.218:sen12ms-human-few-shot-classifier
